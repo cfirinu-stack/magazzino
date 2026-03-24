3384d2c8c1ac87b49f0796c54e359f41dd0f29a9
cfirinu-stack <cfirinu@gmail.com>
2026-03-24 20:26:21 +0100

    Create seeders.sql

---
+++ database/seeders.sql
@@ -0,0 +1,14 @@
+USE warehouse_inventory;
+
+INSERT INTO users (name,email,password)
+VALUES
+(
+'Admin',
+'admin@warehouse.com',
+'$2y$10$wHhP7MZ0Q9pQqZ9Y1o9Z0O8WcO5z0Z0s0r0u0k0u0k0u0k0u0k0u'
+),
+(
+'Operatore',
+'operator@warehouse.com',
+'$2y$10$wHhP7MZ0Q9pQqZ9Y1o9Z0O8WcO5z0Z0s0r0u0k0u0k0u0k0u0k0u'
+);
